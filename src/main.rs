8f679292e1f9495e9d7862ffea95a5e606a8da6a
Vladimir Kalnitsky <klntsky@gmail.com>
2023-07-31 03:18:52 +0400

    Add FPS counter

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 use macroquad::prelude::*;
 use macroquad::color::Color;
 use std::cell::Cell;
+use std::cmp::*;
 use rand_xoshiro::Xoshiro128StarStar;
 use rand_core::SeedableRng;
 use rand_core::RngCore;
 use macroquad::input::*;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 struct Game {
 
@@ -56,7 +58,7 @@ impl Optic {
             self.x_to as f32,
             self.y.get() as f32,
             self.x_from as f32,
-            self.y.get() as f32 + OPTIC_HEIGHT as f32 ,
+            (self.y.get() as f32 + OPTIC_HEIGHT as f32).min(H as f32),
             3.0,
             RED
         );
@@ -65,11 +67,10 @@ impl Optic {
             (self.x_to + self.w_to) as f32,
             self.y.get() as f32,
             (self.x_from + self.w_from) as f32,
-            self.y.get() as f32 + OPTIC_HEIGHT as f32 ,
+            (self.y.get() as f32 + OPTIC_HEIGHT as f32).min(H as f32),
             3.0,
             RED
         );
-
     }
 }
 
@@ -85,9 +86,6 @@ impl Level {
         );
     }
 
-    fn render(&self) {
-    }
-
     fn shift(&mut self, shift: i16) {
         self.optics = self.optics.iter().map(|optic| optic.shift(shift)).collect();
     }
@@ -97,12 +95,12 @@ async fn select_level () -> Option<LevelName> {
     Some(LevelName::L1)
 }
 
-const W: u16 = 800;
-const H: u16 = 800;
+const W: u16 = 500;
+const H: u16 = 600;
 
 const OPTIC_HEIGHT : u16 = 30;
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct Screen {
     area: [bool; W as usize]
 }
@@ -121,14 +119,14 @@ impl Screen {
                         let rel_w = (i - opt.x_to) as f32 / opt.w_to as f32;
                         let rel_x: usize = (opt.x_from +
                                             (rel_w * opt.w_from as f32) as i16) as usize;
-                        (*self).area[rel_x]
-                    } else { (*self).area[i_usize] }
+                        self.area[rel_x % W as usize]
+                    } else { self.area[i_usize] }
                 }
             )
         }
     }
 
-    fn draw(&self, y: i16) {
+    fn draw(&self, y: i16, slim: bool) {
         self.area.iter().enumerate().for_each(
             |(i, flag)|
 
@@ -137,11 +135,18 @@ impl Screen {
                     i as f32,
                     0.0,
                     1.0,
-                    y as f32,
+                    if slim { y as f32 - 1.0 } else { y as f32 },
                     if *flag { ORANGE } else { BLACK })
             }
         );
     }
+}
+fn get_sec () -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_secs()
 }
 
 #[macroquad::main("SHAPES")]
@@ -165,15 +170,18 @@ async fn main() {
 
     let mut level1 = Level {
         optics: vec![
-            Optic { x_from: 10, w_from: 400, x_to: 130, w_to: 100, y: Cell::new(-20) },
-            Optic { x_from: 210, w_from: 100, x_to: 120, w_to: 400, y: Cell::new(-200) },
+            // Optic { x_from: 10, w_from: 400, x_to: 130, w_to: 100, y: Cell::new(-20) },
+            // Optic { x_from: 210, w_from: 100, x_to: 120, w_to: 400, y: Cell::new(-200) },
             Optic { x_from: 110, w_from: 100, x_to: 10, w_to: 400, y: Cell::new(-400) },
         ]
     };
 
-    loop {
+    let mut last = get_sec();
+    let mut frame_count = 0;
+    let mut fps: u64 = 0;
 
-        clear_background(Color{r:200.,g:200.,b:200.,a:0.});
+    loop {
+        // clear_background(Color{r:200.,g:200.,b:200.,a:0.});
 
         level1.step();
 
@@ -191,24 +199,24 @@ async fn main() {
 
         let mut acc_screen = screen.clone();
 
-        screen.draw(H as i16);
+        screen.draw(H as i16, false);
 
         level1.optics.iter().for_each(
             |optic|
             {
-                if optic.y.get() < 0 {
+                if optic.y.get() < - (OPTIC_HEIGHT as i16) {
                     return;
                 }
 
-                let mut temp_screen = acc_screen.clone(); //TODO remove clone
+                let mut temp_screen = acc_screen;
                 for i in (0 .. OPTIC_HEIGHT - 1).rev() {
                     let temp_optic = &optic.get_relative(OPTIC_HEIGHT - i);
                     acc_screen.compute(temp_optic)
-                        .draw(optic.y.get() as i16 + i as i16)
+                        .draw(optic.y.get() as i16 + i as i16, true)
                 }
 
                 acc_screen = acc_screen.compute(&optic);
-                acc_screen.draw(optic.y.get() as i16);
+                acc_screen.draw(optic.y.get() as i16, false);
                 optic.draw();
             }
         );
@@ -222,6 +230,17 @@ async fn main() {
         }
 
         frameCounter += 1;
+
+        if get_sec() == last {
+            frame_count += 1;
+        } else {
+            fps = frame_count;
+            last = get_sec();
+            frame_count = 0;
+        }
+
+        draw_text(fps.to_string().as_str(), 20.0, 20.0, 30.0, RED);
+        draw_text(fps.to_string().as_str(), 21.0, 21.0, 30.0, GREEN);
         next_frame().await
     }
 }
